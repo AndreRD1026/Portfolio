1fd560e3acf06f70c13d320f9d4a5043a1e778b0
André R <davilacesar125@gmail.com>
2025-01-31 21:36:44 -0600

    [UPDATE] Se actualizó información de proyectos

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function firstproject () {
     const project = 'https://github.com/AndreRD1026/SS2_202010816/blob/main/Proyecto/Fase1/Documentacion/Documentacion.md';
     window.open(project, "_blank");
 }
+
+function secondproject () {
+    const project = 'https://github.com/findlesscopy/IA_Proyecto1/blob/master/Documentaci%C3%B3n/Manual%20de%20usuario.md';
+    window.open(project, "_blank");
+}
+
+function thirdproject () {
+    const project = 'https://github.com/AndreRD1026/BD1-202010816';
+    window.open(project, "_blank");
+}

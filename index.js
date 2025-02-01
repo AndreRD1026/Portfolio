c2a1d358cc16ec14a4cb969bd50d5a2f69b2f9bc
André R <davilacesar125@gmail.com>
2025-01-31 18:01:58 -0600

    [UPDATE] Se actualizó contenido del portafolio

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function opencv () {
     const cv = 'Mi_CV.pdf';
     window.open(cv, "_blank");
 }
+
+function firstproject () {
+    const project = 'https://github.com/AndreRD1026/SS2_202010816/blob/main/Proyecto/Fase1/Documentacion/Documentacion.md';
+    window.open(project, "_blank");
+}
